040fe3088cd6b72caf84dea0e92fd77f8dfbc082
F1Zuwu <113283931+F1Zuwu@users.noreply.github.com>
2024-09-30 09:44:10 +0300

    Displayed an article by slug

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const mysql = require("mysql2")
 const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({ extended: true }))
 
+const hbs = require("express-handlebars")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs")
+app.engine("hbs", hbs.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts/"
+}))
+
+app.use(express.static("public"))
+
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -18,6 +29,30 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected to joga_mysql db")
+})
+
+app.get("/", (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render("index", {
+            articles: articles
+        })
+    })
+})
+
+app.get("/article/:slug", (req, res) => {
+    let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`;
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        article = result
+        res.render("article", {
+            article: article
+        })
+    })
 })
 
 app.listen(3000, () => {
